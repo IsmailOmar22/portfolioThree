f5b3eac9f216766caaa36c307e40e7d5feaff30b
Ismail Omar <152703433+IsmailOmar22@users.noreply.github.com>
2025-01-24 17:20:23 +0000

    Implement footer section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
 import Reviews from "./sections/Reviews.jsx";
 import Contact from "./sections/Contact.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
     return (
@@ -15,6 +16,7 @@ const App = () => {
             <Projects/>
             <Reviews/>
             <Contact/>
+            <Footer/>
         </main>
     )
 }
