a28e529ab18495306367ffaac48508109690aae4
Ismail Omar <152703433+IsmailOmar22@users.noreply.github.com>
2025-04-09 14:28:52 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const myProjects = [
         desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
         subdesc:
     'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
-        href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+        href: '',
         texture: '/textures/project/project4.mp4',
         logo: '/assets/project-logo4.png',
         logoStyle: {
